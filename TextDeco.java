d4eefc58b3760bac896fd728a55cbd7ae923594d
Glenn <glenndsilva@rediffmail.com>
2024-11-26 17:42:58 +0530

    TextDeco.java

---
+++ TextDeco.java
@@ -0,0 +1,11 @@
+package designpattern.decorator;
+
+public abstract class TextDeco implements Text {
+	Text text;
+	
+	public TextDeco(Text text) {
+		this.text = text;
+	}
+	
+	public abstract String toText();
+}
